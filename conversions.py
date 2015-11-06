67e4a65685dad4c37c341d929085a06e8cce69e4
binkocd <binkocd@gmail.com>
2015-11-05 21:53:28 -0800

    Intial Commit

---
+++ conversions.py
@@ -0,0 +1,22 @@
+table = {
+    "tbsp": {
+        "tsp": 3,
+        "floz": 2
+    },
+    "tsp": {
+        "floz": 0.166
+    }
+}
+
+
+def convert(quanity, froam, to):
+    """Raises a ValueError if conversion between given units is not defined."""
+    try:
+        return quanity * table[froam][to]
+    except:
+        try:
+            return quanity / table[to][froam]
+        except:
+            raise ValueError(
+                "This conversion is not available."
+                "No conversion from '{}' to '{}'".format(froam, to))
